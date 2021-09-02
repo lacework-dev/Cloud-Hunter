6b87b895a30d1f745f8ee55921f05c6b540fe534
Greg Foss <greg.foss@lacework.net>
2021-09-02 10:25:47 -0600

    multithreading!
    adding multithreading to scale-hunt.sh to quickly test rules across a large number of environments

--- scale-hunt.sh
+++ scale-hunt.sh
@@ -6,13 +6,12 @@
 # greg.foss@lacework.net
 #
 # Use alongside cloud-hunter.py to run queries across multiple environments
-# 
 
-# EDIT THESE VARIABLES:
+# EDIT THESE VARIABLES
 # ==============================
-# How you'd like to display the default environment in output
+# How you'd like to display the default environment within output
 primary_env='company_name'
-# Cloud Hunter file location
+# Cloud Hunter script location
 cloud_hunter='/full/path/to/cloud-hunter.py'
 # ==============================
 
@@ -22,45 +21,58 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m'
 
+count_loop () {
+	env=$(echo $i | cut -d "[" -f 2 | cut -d "]" -f 1);
+	if [[ "$*" != *-r* ]]
+	then
+		output=$($cloud_hunter $@ -r -environment $env);
+	else
+		output=$($cloud_hunter $@ -environment $env);
+	fi
+	if [[ "$env" == *default* ]]
+	then
+		env=$primary_env
+	fi
+	clean_output=$(echo $output | cut -d "]" -f 2 | cut -d " " -f 2 | cut -d " " -f 1);
+	if [[ "$output" == *!* ]]
+	then
+		clean_output=$(echo -e "${RED}0${NC}")
+	elif [[ "$output" == *events* ]]
+	then
+		new_output=$(echo $output | cut -d "]" -f 2 | cut -d "[" -f 3 | cut -d "m" -f 2);
+		clean_output=$(echo -e "${GREEN}$new_output${NC}")
+	else
+		clean_output=$(echo -e "${GREEN}$clean_output${NC}")
+	fi
+	printf "%-10s	|	%10s\n" $env $clean_output
+}
+
+detailed_loop () {
+	env=$(echo $i | cut -d "[" -f 2 | cut -d "]" -f 1);
+	if [[ "$*" != *-r* ]]
+	then
+		output=$($cloud_hunter $@ -r -environment $env | grep -v "Query:\|LaceworkLabs_AWS_CloudHunter\|additional details\|filename.csv")
+	else
+		output=$($cloud_hunter $@ -environment $env | grep -v "Query:\|LaceworkLabs_AWS_CloudHunter\|additional details\|filename.csv")
+	fi
+	if [[ "$env" == *default* ]]
+	then
+		env=$primary_env
+	fi
+	echo -e ${CYAN}$env${NC}
+	echo "$output"
+	echo
+	echo
+}
+
 echo
 if [[ "$*" == *-c* ]]
 then
 	printf "${CYAN}Environment${NC}	|	${CYAN}Hits${NC}\n"
 	echo -e "${BLUE}==============================${NC}"
-	for i in $(grep "\[" ~/.lacework.toml);
-		do env=$(echo $i | cut -d "[" -f 2 | cut -d "]" -f 1);
-		if [[ "$*" != *-r* ]]
-		then
-			output=$($cloud_hunter $@ -r -environment $env);
-		else
-			output=$($cloud_hunter $@ -environment $env);
-		fi
-		if [[ "$env" == *default* ]]
-		then
-			env=$primary_env
-		fi
-		clean_output=$(echo $output | cut -d "]" -f 2 | cut -d " " -f 2 | cut -d " " -f 1);
-		if [[ "$output" == *!* ]]
-		then
-			clean_output=$(echo -e "${RED}0${NC}")
-		elif [[ "$output" == *events* ]]
-		then
-			new_output=$(echo $output | cut -d "]" -f 2 | cut -d "[" -f 3 | cut -d "m" -f 2);
-			clean_output=$(echo -e "${GREEN}$new_output${NC}")
-		else
-			clean_output=$(echo -e "${GREEN}$clean_output${NC}")
-		fi
-		printf "%-10s	|	%10s\n" $env $clean_output
-	done
+	for i in $(grep "\[" ~/.lacework.toml); do count_loop "$@" &  done
+	wait
 else
-	for i in $(grep "\[" ~/.lacework.toml);
-		do env=$(echo $i | cut -d "[" -f 2 | cut -d "]" -f 1);
-		echo -e ${CYAN}$env${NC};
-		if [[ "$*" != *-r* ]]
-		then
-			$cloud_hunter $@ -r -environment $env | grep -v "Query:\|LaceworkLabs_AWS_CloudHunter\|additional details\|filename.csv"
-		else
-			$cloud_hunter $@ -environment $env | grep -v "Query:\|LaceworkLabs_AWS_CloudHunter\|additional details\|filename.csv"
-		fi
-	done
+	for i in $(grep "\[" ~/.lacework.toml); do detailed_loop "$@" & done
+	wait
 fi
